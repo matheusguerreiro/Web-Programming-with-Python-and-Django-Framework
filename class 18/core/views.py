baf6572b959f1b272a1035bb4ddacab7879fe384
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2021-03-03 18:01:04 -0300

    commit class about templates with all summary

--- class 18/core/views.py
+++ class 18/core/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    contexto = {
+        'curso': 'Programação Web com Django Framework.'
+    }
+    return render(request, 'index.html', contexto)
 
 def contato(request):
     return render(request, 'contato.html')
