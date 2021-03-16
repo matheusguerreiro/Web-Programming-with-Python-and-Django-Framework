c0f06e4f9494a057beffff68db68c25db8f2320a
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2021-03-16 18:09:57 -0300

    commit class 23

--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!6%@)v&gzo#+tfz03vqfub8+q^gr9@2o@4af9jj3u!c&p4l$6x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
