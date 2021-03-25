61727db9179aff22ec40461b815f1184e8c2dd83
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2021-03-24 23:43:14 -0300

    commit

--- django1/core/models.py
+++ django1/core/models.py
@@ -7,8 +7,14 @@ class Produto(models.Model):
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=7)
     estoque = models.IntegerField('Estoque')
 
+    def __str__(self):
+        return self.nome
+
+
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=21)
     sobrenome = models.CharField('Sobrenome', max_length=32)
     email = models.EmailField('E-mail', max_length=43)
 
+    def __str__(self):
+        return self.nome
